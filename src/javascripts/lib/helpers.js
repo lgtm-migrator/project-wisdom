f3ef282e73bb46f1f6a177dfa1534099a3a2acca
Stuart Harrison <pezholio@gmail.com>
2020-05-11 14:55:29 +0100

    Remove unused code

--- src/javascripts/lib/helpers.js
+++ src/javascripts/lib/helpers.js
@@ -10,19 +10,6 @@ export function resizeContainer (client, max = Number.POSITIVE_INFINITY) {
 }
 
 /**
- * Helper to render a dataset using the same template function
- * @param {Array} set dataset
- * @param {Function} getTemplate function to generate template
- * @param {String} initialValue any template string prepended
- * @return {String} final template
- */
-export function templatingLoop (set, getTemplate, initialValue = '') {
-  return set.reduce((accumulator, item, index) => {
-    return `${accumulator}${getTemplate(item, index)}`
-  }, initialValue)
-}
-
-/**
  * Render template
  * @param {String} replacedNodeSelector selector of the node to be replaced
  * @param {String} htmlString new html string to be rendered
@@ -32,25 +19,4 @@ export function render (replacedNodeSelector, htmlString) {
   const replacedNode = document.querySelector(replacedNodeSelector)
 
   replacedNode.parentNode.replaceChild(fragment, replacedNode)
-}
-
-/**
- * Helper to escape unsafe characters in HTML, including &, <, >, ", ', `, =
- * @param {String} str String to be escaped
- * @return {String} escaped string
- */
-export function escapeSpecialChars (str) {
-  if (typeof str !== 'string') throw new TypeError('escapeSpecialChars function expects input in type String')
-
-  const escape = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#x27;',
-    '`': '&#x60;',
-    '=': '&#x3D;'
-  }
-
-  return str.replace(/[&<>"'`=]/g, function (m) { return escape[m] })
 }
